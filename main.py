2c3bd4e06f51c9e510280e5f228ee0223b073fb9
Aidin <aidinror@gmail.com>
2024-07-01 18:42:12 +0600

    cors

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from fastapi import FastAPI
 import models
 from database.database import engine
 from routers import auth, user, doctor, dtimeslot, appoint
+from fastapi.middleware.cors import CORSMiddleware
+
 models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router, prefix="/user")
 app.include_router(doctor.router, prefix="/doctor")
